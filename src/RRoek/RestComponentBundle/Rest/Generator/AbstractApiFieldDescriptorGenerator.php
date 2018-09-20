a725763c67372c48b9eae81a41b45be968ad7e99
Roy Roek <contact@lemonways.fr>
2018-09-20 11:05:46 +0200

    ft(#RestComponentBundle/Fix): Add ApiFieldDescriptorGenerator

---
+++ src/RRoek/RestComponentBundle/Rest/Generator/AbstractApiFieldDescriptorGenerator.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace RRoek\RestComponentBundle\Rest\Generator;
+
+use RRoek\RestComponentBundle\Rest\ListBuilder\Doctrine\FieldDescriptor\DoctrineFieldDescriptor;
+use RRoek\RestComponentBundle\Rest\ListBuilder\Doctrine\FieldDescriptor\DoctrineGroupConcatFieldDescriptor;
+use RRoek\RestComponentBundle\Rest\ListBuilder\Doctrine\FieldDescriptor\DoctrineJoinDescriptor;
+use Traversable;
+
+/**
+ * Class AbstractApiFieldDescriptorGenerator.
+ */
+class AbstractApiFieldDescriptorGenerator
+{
+    // ---- --- Private & Protected methods--- ----
+    /**
+     * Returns array of existing field-descriptors.
+     *
+     * @param Traversable $entityFields
+     * @param string      $entityName
+     *
+     * @return array
+     */
+    protected function getSimpleFieldDescriptors($entityFields, $entityName)
+    {
+        $results = [];
+        foreach ($entityFields as $field) {
+            if (is_array($field)) {
+                $results[$field['field']] = new DoctrineFieldDescriptor(
+                    $field['field'],
+                    $field['delimitor'] . $field['field'],
+                    $entityName,
+                    $field['field'],
+                    []
+                );
+            } else {
+                $results[$field] = new DoctrineFieldDescriptor(
+                    $field,
+                    $field,
+                    $entityName,
+                    $field,
+                    []
+                );
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Returns array of existing field-descriptors.
+     *
+     * @param Traversable $joinEntityFields
+     * @param string      $entityName
+     *
+     * @return array
+     */
+    protected function getJoinedFieldDescriptors($joinEntityFields, $entityName)
+    {
+        $results = [];
+        foreach ($joinEntityFields as $joinEntityClassName => $fields) {
+            if (is_array($fields)) {
+                $joinToLinkedEntity
+                    = self::getDynamicEntityJoin($joinEntityClassName, $entityName, false);
+
+                foreach ($fields as $field) {
+                    if (isset($field['joinedEntitySearchedFieldName'])) {
+                        $results[$field['joinedEntitySearchedFieldName']] = new DoctrineFieldDescriptor(
+                            $field['joinedEntitySearchedField'],
+                            $field['joinedEntitySearchedFieldName'],
+                            $joinEntityClassName,
+                            $field['field'],
+                            $joinToLinkedEntity
+                        );
+                    } else {
+                        $results[$field['field']] = new DoctrineFieldDescriptor(
+                            $field['joinedEntitySearchedField'],
+                            $field['field'],
+                            $joinEntityClassName,
+                            $field['field'],
+                            $joinToLinkedEntity
+                        );
+                    }
+                }
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Returns array of existing field-descriptors.
+     *
+     * @param Traversable $joinEntityFields
+     * @param string      $entityName
+     *
+     * @return array
+     */
+    protected function getOneToManyJoinedFieldDescriptors($joinEntityFields, $entityName)
+    {
+        $results = [];
+        foreach ($joinEntityFields as $joinEntityClassName => $field) {
+            if (is_array($field)) {
+                $joinToLinkedEntity       =
+                    self::getDynamicEntityJoin($joinEntityClassName, $entityName, true);
+                $results[$field["field"]] = new DoctrineGroupConcatFieldDescriptor(
+                    new DoctrineFieldDescriptor(
+                        $field["joinedEntitySearchedField"],
+                        $field["field"],
+                        $joinEntityClassName,
+                        $field["field"],
+                        $joinToLinkedEntity
+                    ),
+                    $field["field"],
+                    null,
+                    ',',
+                    false,
+                    false,
+                    '',
+                    '',
+                    '',
+                    true,
+                    false,
+                    '',
+                    true
+                );
+            } else {
+                $joinToLinkedEntity
+                                 = self::getDynamicEntityJoin($joinEntityClassName, $entityName, true);
+                $results[$field] = new DoctrineGroupConcatFieldDescriptor(
+                    new DoctrineFieldDescriptor(
+                        'id',
+                        $field,
+                        $joinEntityClassName,
+                        $field,
+                        $joinToLinkedEntity
+                    ),
+                    $field
+                );
+            }
+        }
+
+        return $results;
+    }
+
+    //---- --- Public Methods --- ----
+    /**
+     * @param string $targetEntityJoin
+     * @param string $mainEntityName
+     * @param bool   $multiple
+     * @param null   $joinCondition
+     * @param string $joinMethod
+     *
+     * @return array
+     */
+    public static function getDynamicEntityJoin($targetEntityJoin, $mainEntityName, $targetPropertyOnEntityPlurialName = null, $multiple = false, $joinCondition = null, $joinMethod = DoctrineJoinDescriptor::JOIN_METHOD_LEFT)
+    {
+        $propertyOnEntity = lcfirst(substr($targetEntityJoin, (strpos($targetEntityJoin, ':') ?: -1) + 1));
+         if (null !== $targetPropertyOnEntityPlurialName) {
+             return [
+                 $targetEntityJoin => new DoctrineJoinDescriptor(
+                     $targetEntityJoin,
+                     $mainEntityName . '.' . (true === $multiple ? $targetPropertyOnEntityPlurialName . 's' : $propertyOnEntity),
+                     $joinCondition,
+                     $joinMethod
+                 ),
+             ];
+        } else {
+             return [
+                $targetEntityJoin => new DoctrineJoinDescriptor(
+                    $targetEntityJoin,
+                    $mainEntityName . '.' . (true === $multiple ? $propertyOnEntity . 's' : $propertyOnEntity),
+                    $joinCondition,
+                    $joinMethod
+                ),
+            ];
+        }
+    }
+
+    /**
+     * @param      $targetEntityJoin
+     * @param bool $multiple
+     *
+     * @return array
+     */
+    public static function getDynamicSelfEntityJoin($targetEntityJoin, $multiple = false)
+    {
+        if (!$multiple) {
+            return [
+                $targetEntityJoin . 'Parent' => new DoctrineJoinDescriptor(
+                    $targetEntityJoin,
+                    $targetEntityJoin . '.parent'
+                ),
+            ];
+        } else {
+            return [
+                $targetEntityJoin . 'Children' => new DoctrineJoinDescriptor(
+                    $targetEntityJoin,
+                    $targetEntityJoin . '.children'
+                ),
+            ];
+        }
+    }
+}
---
+++ src/RRoek/RestComponentBundle/Rest/Generator/ApiFieldDescriptorGenerator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace RRoek\RestComponentBundle\Rest\Generator;
+
+use Traversable;
+
+/**
+ * Class ApiFieldDescriptorGenerator.
+ */
+class ApiFieldDescriptorGenerator extends AbstractApiFieldDescriptorGenerator
+{
+    //---- --- Public Methods --- ----
+    /**
+     * @param Traversable $entityFields
+     *
+     * @return array
+     */
+    public function getAllFieldDescriptors($entityFields)
+    {
+        $simpleFields          = $this->getSimpleFieldDescriptors(
+            $entityFields['entityFields'],
+            $entityFields['entityName']
+        );
+        $manyToOneJoinedFields = $this->getJoinedFieldDescriptors(
+            $entityFields['joinManyToOneEntityFields'],
+            $entityFields['entityName']
+        );
+        $oneToOneJoinedFields  = $this->getJoinedFieldDescriptors(
+            $entityFields['joinOneToOneEntityFields'],
+            $entityFields['entityName']
+        );
+        $oneToManyJoinedFields = $this->getOneToManyJoinedFieldDescriptors(
+            $entityFields['joinOneToManyEntityFields'],
+            $entityFields['entityName']
+        );
+
+        return array_merge($simpleFields, $manyToOneJoinedFields, $oneToOneJoinedFields, $oneToManyJoinedFields);
+    }
+}
